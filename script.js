011116fc94bd1cfc01f2b31a777ab78c8920ce09
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2023-12-06 16:33:53 +0100

    upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ fetch("https://striveschool-api.herokuapp.com/books")
     if (responseObj.ok) {
       return responseObj.json();
     }
+    throw new Error("Errore nella risposta del server");
   })
   .then((booksObj) => {
     const row = document.querySelector(".row");
@@ -18,6 +19,7 @@ fetch("https://striveschool-api.herokuapp.com/books")
             <div class="card-body">
               <h5 class="card-title">${book.title}</h5>
               <p class="card-text">Price: $${book.price}</p>
+              <button class="btn btn-success" onclick="purchaseBook('${book.asin}')">Compra</button>
               <button class="btn btn-danger" onclick="deleteBook('${book.asin}')">Scarta</button>
             </div>
           </div>
@@ -28,8 +30,26 @@ fetch("https://striveschool-api.herokuapp.com/books")
   .catch((error) => {
     console.error("Errore durante il recupero dei dati:", error);
   });
+// const cart = [];
+// let booksObj;
 
 const deleteBook = (id) => {
   const cardToRemove = document.getElementById(id);
   cardToRemove.remove();
 };
+
+// const updateCartCount = () => {
+//   const cartCount = document.getElementById("cartCount");
+//   cartCount.textContent = cart.length;
+// };
+
+// const showCart = () => {
+//   console.log("Contenuto del carrello:", cart);
+// };
+
+// const purchaseBook = (id, books) => {
+//   const selectedBook = books.find((book) => book.asin === id);
+//   cart.push(selectedBook);
+//   console.log("Libro aggiunto al carrello:", selectedBook);
+//   updateCartCount();
+// };
